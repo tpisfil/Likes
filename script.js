f6437ab28c78d96a38d31c2aed53d1f80dee99b9
tpisfil <tpisfil13@gmail.com>
2021-04-28 06:15:48 -0400

    commit

---
+++ script.js
@@ -0,0 +1,17 @@
+var neilLikes = 9;
+function likeNeil(){
+    neilLikes ++;
+    document.querySelector("#like1").innerText = neilLikes + " Like(s)";
+}
+
+var nicholeLikes = 12;
+function likeNichole(){
+    nicholeLikes ++;
+    document.querySelector("#like2").innerText = nicholeLikes + " Like(s)";
+}
+
+var jimLikes = 9;
+function likeJim(){
+    jimLikes ++;
+    document.querySelector("#like3").innerText = jimLikes + " Like(s)";
+}
